570b493c797aa601b3b2683aaacab9cee8f1e5f7
gnembon <gnembonmc@gmail.com>
2019-11-12 13:46:16 -0400

    initial commit based on carpet example extension

---
+++ src/main/java/carpet_client/CarpetClient.java
@@ -0,0 +1,55 @@
+package carpet_client;
+
+import carpet.CarpetExtension;
+import carpet.CarpetServer;
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class CarpetClient implements CarpetExtension
+{
+    public static void noop() { }
+    static
+    {
+        CarpetServer.manageExtension(new CarpetClient());
+    }
+
+    @Override
+    public void onGameStarted()
+    {
+        // let's /carpet handle our few simple settings
+        CarpetServer.settingsManager.parseSettingsClass(CarpetClientSettings.class);
+    }
+
+    @Override
+    public void onServerLoaded(MinecraftServer server)
+    {
+        // reloading of /carpet settings is handled by carpet
+        // reloading of own settings is handled as an extension, since we claim own settings manager
+    }
+
+    @Override
+    public void onTick(MinecraftServer server)
+    {
+        // no need to add this.
+    }
+
+    @Override
+    public void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher)
+    {
+        // client commands
+    }
+
+    @Override
+    public void onPlayerLoggedIn(ServerPlayerEntity player)
+    {
+        //
+    }
+
+    @Override
+    public void onPlayerLoggedOut(ServerPlayerEntity player)
+    {
+        //
+    }
+}
---
+++ src/main/java/carpet_client/CarpetClientSettings.java
@@ -0,0 +1,20 @@
+package carpet_client;
+
+import carpet.settings.ParsedRule;
+import carpet.settings.Rule;
+import carpet.settings.Validator;
+import carpet.utils.Messenger;
+import net.minecraft.server.command.ServerCommandSource;
+
+import static carpet.settings.RuleCategory.CREATIVE;
+
+/**
+ * Here is your example Settings class you can plug to use carpetmod /carpet settings command
+ */
+public class CarpetClientSettings
+{
+    /**
+     *  Custom validator class for your setting. If validate returns null - settings is not changed.
+     */
+
+}
